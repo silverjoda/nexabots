a975b687a45a87bade37c9678965bb7374776055
silverjoda <heizen.sh@gmail.com>
2018-12-19 17:06:41 +0100

    m

---
+++ src/algos/ES/train_ES.py
@@ -0,0 +1,198 @@
+import numpy as np
+import cma
+from time import sleep
+import torch
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.convert_parameters import vector_to_parameters, parameters_to_vector
+import time
+import mujoco_py
+
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+import multiprocessing as mp
+import os
+from copy import deepcopy
+
+class LinearPolicy(nn.Module):
+    def __init__(self, obs_dim, act_dim):
+        super(LinearPolicy, self).__init__()
+        self.fc1 = nn.Linear(obs_dim, act_dim)
+
+    def forward(self, x):
+        x = T.tanh(self.fc1(x))
+        return x
+
+
+class NN(nn.Module):
+    def __init__(self, obs_dim, act_dim):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(obs_dim, 32)
+        self.fc2 = nn.Linear(32, 12)
+        self.fc3 = nn.Linear(12, act_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = T.tanh(self.fc3(x))
+        return x
+
+
+def f_wrapper(env, policy, animate):
+    def f(w):
+        reward = 0
+        done = False
+        obs, _ = env.reset()
+
+        vector_to_parameters(torch.from_numpy(w).float(), policy.parameters())
+
+        while not done:
+
+            # Get action from policy
+            with torch.no_grad():
+                act = policy(torch.from_numpy(np.expand_dims(obs, 0)))[0].numpy()
+
+            # Step environment
+            obs, rew, done, _ = env.step(act)
+
+            if animate:
+                env.render()
+
+            reward += rew
+
+        return -reward
+    return f
+
+
+def f_mp(pos, env_fun, sim, policy, w, output):
+    env = env_fun(animate=False, sim=sim)
+
+    reward = 0
+    done = False
+    obs, _ = env.reset()
+
+    vector_to_parameters(torch.from_numpy(w).float(), policy.parameters())
+
+    while not done:
+
+        # Get action from policy
+        with torch.no_grad():
+            act = policy(torch.from_numpy(np.expand_dims(obs, 0)))[0].numpy()
+
+        # Step environment
+        obs, rew, done, _ = env.step(act)
+
+        reward += rew
+
+    output.put((pos, -reward))
+
+
+def train(params):
+    env_fun, iters, animate = params
+    env = env_fun(animate=animate)
+    obs_dim, act_dim = env.obs_dim, env.act_dim
+    policy = NN(obs_dim, act_dim).float()
+    w = parameters_to_vector(policy.parameters()).detach().numpy()
+    es = cma.CMAEvolutionStrategy(w, 0.5)
+    f = f_wrapper(env, policy, animate)
+
+    print("Env: {} Action space: {}, observation space: {}, N_params: {}, comments: ...".format(env_fun.__name__, act_dim,
+                                                                                                obs_dim, len(w)))
+    it = 0
+    try:
+        while not es.stop():
+            it += 1
+            if it > iters:
+                break
+            if it % 1000 == 0:
+                sdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                    "agents/{}.p".format(env_fun.__name__))
+                vector_to_parameters(torch.from_numpy(es.result.xbest).float(), policy.parameters())
+                T.save(policy, sdir)
+                print("Saved checkpoint")
+            X = es.ask()
+            es.tell(X, [f(x) for x in X])
+            es.disp()
+    except KeyboardInterrupt:
+        print("User interrupted process.")
+
+    return es.result.fbest
+
+
+def train_mt(params):
+    env_fun, iters, _ = params
+    env = env_fun(animate=False)
+    obs_dim, act_dim = env.obs_dim, env.act_dim
+
+    policy = NN(obs_dim, act_dim).float()
+    w = parameters_to_vector(policy.parameters()).detach().numpy()
+    es = cma.CMAEvolutionStrategy(w, 0.5)
+
+    print("Env: {} Action space: {}, observation space: {}, N_params: {}, comments: ...".format("Ant_reach", act_dim,
+                                                                                              obs_dim, len(w)))
+
+    model = mujoco_py.load_model_from_path(env_fun.MODELPATH)
+
+    sims = [mujoco_py.MjSim(model) for _ in range(es.popsize)]
+    policies = [policy] * es.popsize
+
+    ctr = 0
+    try:
+        while not es.stop():
+            ctr += 1
+            if ctr > iters:
+                break
+            if ctr % 1000 == 0:
+                sdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                    "agents/{}.p".format(env_fun.__name__))
+                vector_to_parameters(torch.from_numpy(es.result.xbest).float(), policy.parameters())
+                T.save(policy, sdir)
+                print("Saved checkpoint")
+            X = es.ask()
+
+            output = mp.Queue()
+            processes = []
+            for i, ef, sim, policy, x in zip(range(es.popsize), [env_fun] * es.popsize, sims, policies, X):
+                processes.append(mp.Process(target=f_mp, args=(i, ef, sim, policy, x, output)))
+
+            # Run processes
+            for p in processes:
+                p.start()
+
+            # Exit the completed processes
+            for p in processes:
+                p.join()
+
+            evals = [output.get() for _ in processes]
+            evals.sort(key=lambda x : x[0])
+            evals = [ev[1] for ev in evals]
+
+            es.tell(X, evals)
+            es.disp()
+    except KeyboardInterrupt:
+        print("User interrupted process.")
+
+    return es.result.fbest
+
+
+from src.envs.ant_reach_mjc.ant_reach_mjc import AntReachMjc as env_fun
+T.set_num_threads(1)
+
+if False:
+    policy = T.load("agents/AntTerrainMjc.p")
+    env_fun().test(policy)
+    exit()
+
+t1 = time.clock()
+train((env_fun, 1001, True))
+t2 = time.clock()
+print("Elapsed time: {}".format(t2 - t1))
+
+# TODO: Modify this file to train for simple envs with homogeneous obs
+# TODO: Make mjc centipede env
+# TODO: Make mjc antreach env
+# TODO: Launch training on GUDLE
+
+
+
---
+++ src/envs/ant_reach_mjc/ant_reach_mjc.py
@@ -0,0 +1,198 @@
+import time
+import os
+import numpy as np
+import mujoco_py
+from collections import deque
+
+class AntReachMjc:
+    MODELPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ant_reach.xml")
+
+    def __init__(self, animate=False, sim=None):
+        if sim is not None:
+            self.sim = sim
+            self.model = self.sim.model
+        else:
+            self.modelpath = AntReachMjc.MODELPATH
+            self.model = mujoco_py.load_model_from_path(self.modelpath)
+            self.sim = mujoco_py.MjSim(self.model)
+
+        self.model.opt.timestep = 0.02
+
+        # Environment dimensions
+        self.q_dim = self.sim.get_state().qpos.shape[0]
+        self.qvel_dim = self.sim.get_state().qvel.shape[0]
+
+        self.obs_dim = self.q_dim + self.qvel_dim
+        self.act_dim = self.sim.data.actuator_length.shape[0]
+
+        # Environent inner parameters
+        self.success_queue = deque(maxlen=100)
+        self.viewer = None
+        self.goal = None
+        self.current_pose = None
+        self.success_rate = 0
+        self.step_ctr = 0
+
+        # Initial methods
+        self.reset()
+        self.setupcam()
+
+
+    def setupcam(self):
+        if self.viewer is None:
+            self.viewer = mujoco_py.MjViewer(self.sim)
+        self.viewer.cam.trackbodyid = -1
+        self.viewer.cam.distance = self.model.stat.extent * 1.3
+        self.viewer.cam.lookat[0] = -0.1
+        self.viewer.cam.lookat[1] = 0
+        self.viewer.cam.lookat[2] = 0.5
+        self.viewer.cam.elevation = -20
+
+
+    def get_obs(self):
+        qpos = self.sim.get_state().qpos.tolist()
+        qvel = self.sim.get_state().qvel.tolist()
+        a = qpos + qvel
+        return np.asarray(a, dtype=np.float32)
+
+
+    def get_obs_dict(self):
+        od = {}
+        for j in self.sim.model.joint_names:
+            od[j + "_pos"] = self.sim.data.get_joint_qpos(j)
+            od[j + "_vel"] = self.sim.data.get_joint_qvel(j)
+        return od
+
+
+    def get_state(self):
+        return self.sim.get_state()
+
+
+    def set_state(self, qpos, qvel=None):
+        qvel = np.zeros(self.q_dim) if qvel is None else qvel
+        old_state = self.sim.get_state()
+        new_state = mujoco_py.MjSimState(old_state.time, qpos, qvel,
+                                         old_state.act, old_state.udd_state)
+        self.sim.set_state(new_state)
+        self.sim.forward()
+
+
+    def _sample_goal(self, pose):
+        while True:
+            x, y = pose
+            nx = x + np.random.randn() * (2. + 3 * self.success_rate)
+            ny = y + np.random.randn() * (2. + 3 * self.success_rate)
+
+            goal = nx, ny
+
+            if not self.reached_goal(pose, goal):
+                break
+
+        return np.array(goal)
+
+
+    def _update_stats(self, reached_goal):
+        self.success_queue.append(1. if reached_goal else 0.)
+        self.success_rate = np.mean(self.success_queue)
+
+
+    def reached_goal(self, pose, goal):
+        x,y = pose
+        xg,yg = goal
+        return (x-xg)**2 < 0.2 and (y-yg)**2 < 0.2
+
+
+    def render(self, human=True):
+        if self.viewer is None:
+            self.viewer = mujoco_py.MjViewer(self.sim)
+        if not human:
+            return self.sim.render(camera_name=None,
+                                   width=224,
+                                   height=224,
+                                   depth=False)
+            #return viewer.read_pixels(width, height, depth=False)
+
+        self.viewer.render()
+
+
+    def step(self, ctrl):
+
+        self.sim.data.ctrl[:] = ctrl
+        self.sim.forward()
+        self.sim.step()
+
+        print(self.sim.data.ncon)
+
+        self.step_ctr += 1
+
+        obs = self.get_obs()
+
+        # Make relevant pose from observation (x,y)
+        x, y, z, q1, q2, q3, q4 = self.sim.data.get_joint_qpos("root")
+        pose = (x, y)
+
+        prev_dist  = np.sqrt(np.sum((np.asarray(self.current_pose) - np.asarray(self.goal))**2))
+        current_dist = np.sqrt(np.sum((np.asarray(pose) - np.asarray(self.goal))**2))
+
+        # Check if goal has been reached
+        reached_goal = self.reached_goal(pose, self.goal)
+
+        # Reevaluate termination condition
+        done = reached_goal or self.step_ctr > 400
+
+        if reached_goal:
+            print("SUCCESS")
+
+        # Update success rate
+        if done:
+            self._update_stats(reached_goal)
+
+        ctrl_effort = np.square(ctrl).sum() * 0.03
+        target_progress = (prev_dist - current_dist) * 70
+        target_trueness = 0
+
+        r = target_progress - ctrl_effort
+
+        self.current_pose = pose
+
+        return obs, r, done, None
+
+
+    def demo(self):
+        self.reset()
+        for i in range(1000):
+            self.step(self.action_space.sample())
+            self.render()
+
+
+    def reset(self):
+
+        # Reset env variables
+        self.step_ctr = 0
+
+        # Sample initial configuration
+        init_q = np.zeros(self.q_dim, dtype=np.float32)
+        init_q[0] = np.random.randn() * 0.1
+        init_q[1] = np.random.randn() * 0.1
+        init_q[2] = 0.60 + np.random.rand() * 0.1
+        init_qvel = np.random.randn(self.qvel_dim).astype(np.float32) * 0.1
+
+        obs = np.concatenate((init_q, init_qvel))
+
+        self.current_pose = init_q[0:2]
+        self.goal = self._sample_goal(self.current_pose)
+
+        # Set object position
+        init_q[self.q_dim - 2:] = self.goal
+
+        # Set environment state
+        self.set_state(init_q, init_qvel)
+
+        return obs
+
+
+if __name__ == "__main__":
+    ant = AntReachMjc(animate=True)
+    print(ant.obs_dim)
+    print(ant.act_dim)
+    ant.demo()

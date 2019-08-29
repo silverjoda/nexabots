ed6b5418c292d394cdcb1b7ddfe6ec3c948d2261
silverjoda <heizen.sh@gmail.com>
2019-08-29 11:03:40 +0200

    c

--- src/algos/PG/pg.py
+++ src/algos/PG/pg.py
@@ -270,7 +270,7 @@ if __name__=="__main__":
 
     ID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
     params = {"iters": 500000, "batchsize": 50, "gamma": 0.995, "policy_lr": 0.0007, "weight_decay" : 0.0001, "ppo": True,
-              "ppo_update_iters": 6, "animate": True, "train" : True, "env_list" : env_list,
+              "ppo_update_iters": 6, "animate": False, "train" : True, "env_list" : env_list,
               "note" : "Ant feelers goal mjc", "ID" : ID}
 
     if socket.gethostname() == "goedel":
@@ -280,8 +280,11 @@ if __name__=="__main__":
     #from src.envs.centipede.centipede import Centipede
     #env = Centipede(4)
 
-    from src.envs.ant_feelers_mem_mjc.ant_feelers_goal_mem_mjc import AntFeelersMjc
-    env = AntFeelersMjc()
+    #from src.envs.ant_feelers_mem_mjc.ant_feelers_goal_mem_mjc import AntFeelersMjc
+    #env = AntFeelersMjc()
+
+    from src.envs.locom_benchmarks.flat_env.hex_flat import Hexapod
+    env = Hexapod()
 
     # Test
     if params["train"]:
--- src/envs/locom_benchmarks/flat_env/hex_flat.py
+++ src/envs/locom_benchmarks/flat_env/hex_flat.py
@@ -21,10 +21,13 @@ class Hexapod(gym.Env):
         self.joints_rads_high = np.array([0.6, 0.4, 0.8] * 6)
         self.joints_rads_diff = self.joints_rads_high - self.joints_rads_low
 
+        self.target_vel = 0.4 # Target velocity with which we want agent to move
+        self.max_steps = 200
+
         self.reset()
 
-        self.observation_space = spaces.Box(low=-1, high=1, dtype=np.float32, shape=(self.obs_dim,))
-        self.action_space = spaces.Box(low=-1, high=1, dtype=np.float32, shape=(self.act_dim,))
+        #self.observation_space = spaces.Box(low=-1, high=1, dtype=np.float32, shape=(self.obs_dim,))
+        #self.action_space = spaces.Box(low=-1, high=1, dtype=np.float32, shape=(self.act_dim,))
 
         self.setupcam()
 
@@ -57,6 +60,20 @@ class Hexapod(gym.Env):
         return (np.array(action) * 0.5 + 0.5) * self.joints_rads_diff + self.joints_rads_low
 
 
+    def scale_joints(self, joints):
+        return ((np.array(joints) - self.joints_rads_low) / self.joints_rads_diff) * 2 - 1
+
+
+    def get_agent_obs(self):
+        qpos = self.sim.get_state().qpos.tolist()
+        qvel = self.sim.get_state().qvel.tolist()
+        contacts = (np.abs(np.array(self.sim.data.cfrc_ext[[4, 7, 10, 13, 16, 19]])).sum(axis=1) > 0.05).astype(
+            np.float32)
+
+        # Joints, joint velocities, quaternion, pose velocities (xd,yd,zd,thd,phd,psid), foot contacts
+        return np.concatenate((self.scale_joints(qpos[7:]), qvel[6:], qpos[3:7], qvel[:6], contacts))
+
+
     def step(self, ctrl):
         # Clip control signal
         ctrl = np.clip(ctrl, -1, 1)
@@ -67,60 +84,31 @@ class Hexapod(gym.Env):
         # Scale control according to joint ranges
         ctrl = self.scale_action(ctrl)
 
-        # TODO: Continue building the first environment, then copy to all the rest of the blind ones, then do rangefinder and feelers, then heterogeneous environments, then camera
+        # TODO: !Continue building the first environment!, then copy to all the rest of the blind ones, then do rangefinder and feelers, then heterogeneous environments, then camera
 
+        # Step the simulator
         self.sim.data.ctrl[:] = ctrl
         self.sim.forward()
         self.sim.step()
         self.step_ctr += 1
 
-        obs = self.get_obs()
-
-        # Angle deviation
-        x, y, z, qw, qx, qy, qz = obs[:7]
+        # Get agent telemetry data
+        _, _, _, qw, qx, qy, qz = self.sim.get_state().qpos.tolist()[:7]
         xd, yd, zd, thd, phid, psid = self.sim.get_state().qvel.tolist()[:6]
-        # xa, ya, za, tha, phia, psia = self.sim.data.qacc.tolist()[:6]
-
-        self.vel_sum += xd
 
         # Reward conditions
-        target_vel = 0.4
-        # avg_vel = self.vel_sum / self.step_ctr
-
-        velocity_rew = 1. / (abs(xd - target_vel) + 1.) - 1. / (target_vel + 1.)
-        velocity_rew = velocity_rew * (1 / (1 + 30 * np.square(yd)))
-
-        roll, pitch, yaw = my_utils.quat_to_rpy([qw, qx, qy, qz])
-        yaw_deviation = np.min((abs((yaw % 6.183) - (0 % 6.183)), abs(yaw - 0)))
-
+        velocity_rew = 1. / (abs(xd - self.target_vel) + 1.) - 1. / (self.target_vel + 1.)
         q_yaw = 2 * acos(qw)
 
-        r_neg = np.square(y) * 0.2 + \
-                np.square(q_yaw) * 0.5 + \
-                np.square(pitch) * 0.5 + \
-                np.square(roll) * 0.5 + \
-                np.square(ctrl_pen) * 0.1 + \
-                np.square(zd) * 0.7
-
-        r_pos = velocity_rew * 6  # + (abs(self.prev_yaw_deviation) - abs(yaw_deviation)) * 3. + (abs(self.prev_y_deviation) - abs(y)) * 3.
-        r = r_pos - r_neg
-
-        self.prev_yaw_deviation = yaw_deviation
-        self.prev_y_deviation = y
+        r = velocity_rew * 10 - \
+            np.square(q_yaw) * 0.5 - \
+            np.square(ctrl_pen) * 0.1 - \
+            np.square(zd) * 0.7
 
         # Reevaluate termination condition
         done = self.step_ctr > self.max_steps  # or abs(y) > 0.3 or abs(yaw) > 0.6 or abs(roll) > 0.8 or abs(pitch) > 0.8
-        contacts = (np.abs(np.array(self.sim.data.cfrc_ext[[4, 7, 10, 13, 16, 19]])).sum(axis=1) > 0.05).astype(
-            np.float32)
 
-
-        obs = np.concatenate([self.scale_joints(self.sim.get_state().qpos.tolist()[7:]),
-                              self.sim.get_state().qvel.tolist()[6:],
-                              self.sim.get_state().qvel.tolist()[:6],
-                              [qw, qx, qy, qz, y],
-                              contacts])
-
-        return obs, r, done, None
+        return self.get_agent_obs(), r, done, None
 
 
     def reset(self):
@@ -138,19 +126,18 @@ class Hexapod(gym.Env):
         self.q_dim = self.sim.get_state().qpos.shape[0]
         self.qvel_dim = self.sim.get_state().qvel.shape[0]
 
-        self.obs_dim = 18 * 2 + 4 + 6 + 6 # j, jd, quat, pose_velocity, contacts
+        self.obs_dim = 18 + 18 + 4 + 6 + 6 # j, jd, quat, pose_velocity, contacts
         self.act_dim = self.sim.data.actuator_length.shape[0]
 
         # Set initial position
         init_q = np.zeros(self.q_dim, dtype=np.float32)
         init_q[0] = 0.0
         init_q[1] = 0.0
-        init_q[2] = 0.50
+        init_q[2] = 0.10
         init_qvel = np.random.randn(self.qvel_dim).astype(np.float32) * 0.1
 
         # Set environment state
         self.set_state(init_q, init_qvel)
-
         self.step_ctr = 0
 
         obs, _, _, _ = self.step(np.zeros(self.act_dim))

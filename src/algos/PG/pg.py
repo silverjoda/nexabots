79295e4483b2c0348d916f15e96d0acb764197cf
silverjoda <heizen.sh@gmail.com>
2019-09-02 14:48:03 +0200

    c

--- src/algos/PG/pg.py
+++ src/algos/PG/pg.py
@@ -270,7 +270,7 @@ if __name__=="__main__":
 
     ID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
     params = {"iters": 500000, "batchsize": 50, "gamma": 0.995, "policy_lr": 0.0007, "weight_decay" : 0.0001, "ppo": True,
-              "ppo_update_iters": 6, "animate": True, "train" : False, "env_list" : env_list,
+              "ppo_update_iters": 6, "animate": True, "train" : True, "env_list" : env_list,
               "note" : "--", "ID" : ID}
 
     if socket.gethostname() == "goedel":
@@ -288,7 +288,7 @@ if __name__=="__main__":
     #from src.envs.locom_benchmarks.quad_locomotion.quad_blind import Quad as env
     #from src.envs.locom_benchmarks.snake_locomotion.snake_blind import Snake as env
 
-    env = env([hf_gen.flat], 1)
+    env = env([hf_gen.perlin], 1)
 
     # Test
     if params["train"]:
